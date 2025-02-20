460c9d0f75391bed677fc678c9b1abc1811364c7
RahulSahOfficial <developersah@gmail.com>
2025-02-20 13:02:36 +0530

    Added Starting Files

---
+++ README.txt
@@ -0,0 +1,3 @@
+# Server Directory Info and Tech Stack
+
+# Client Dierctory Info and Tech Stack
---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1>School Automation</h1>
+    </>
+  )
+}
+
+export default App
